0d7f06745ebcceb81e7877770ee91e6bc9a33cdd
Ahmer Gondal <ajg2204@hsutx.edu>
2023-10-08 19:22:33 -0500

    Error logging

--- src/pages/AnimeStream.jsx
+++ src/pages/AnimeStream.jsx
@@ -4,8 +4,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import React from "react";
 // import DiscussionPage from "./DiscussionPage";
-import "./../components/css/AnimeStream.css"
-
+import "./../components/css/AnimeStream.css";
 
 export default function AnimeStream() {
   const [data, setData] = useState("");
@@ -15,39 +14,40 @@ export default function AnimeStream() {
   const { episodeId, animeId } = useParams();
   // const [comments, setComments] = useState([]);
 
-
   const getAnimeStream = async () => {
     try {
       const animeVid = await axios.get(
         `https://api.amvstr.ml/api/v2/stream/${episodeId}`
       );
-       setPlayer(animeVid?.data?.data?.player?.main)
-       setData(animeVid?.data?.data?.plyr?.backup);
+      console.log(animeVid);
+      setPlayer(animeVid?.data?.data?.player?.main);
+      setData(animeVid?.data?.data?.plyr?.backup);
+      console.log(episodeId, animeId);
       //  const response = await axios.get(`/api/discussions?episodeId=${episodeId}`);
       //  setComments(response.data.discussions);
     } catch (err) {
       console.error(err);
-      return { error: 'Could not show anime stream'};
+      return { error: "Could not show anime stream" };
     }
-  }
+  };
 
   const handleDisplay = () => {
     setDisplay(false);
-  }
+  };
 
   const handlePlayer = () => {
-    setDisplay(true)
-  }
+    setDisplay(true);
+  };
   const getAnimeDetails = async () => {
     try {
-      const api = await fetch(`${AniApi}/meta/anilist/info/${animeId}`)
-      const res = await api.json()
+      const api = await fetch(`${AniApi}/meta/anilist/info/${animeId}`);
+      const res = await api.json();
       setDetails(res);
     } catch (err) {
       console.error(err);
-      return { error: 'Could not show anime details'};
+      return { error: "Could not show anime details" };
     }
-  }
+  };
   useEffect(() => {
     getAnimeDetails();
     getAnimeStream();
@@ -73,27 +73,29 @@ export default function AnimeStream() {
                   allowFullScreen="allowfullscreen"
                   title={episodeId}
                   allow="picture-in-picture"
-                  webkitallowfullscreen="true"></iframe>
+                  webkitallowfullscreen="true"
+                ></iframe>
               ) : (
                 <iframe
-                src={data}
-                allowFullScreen="allowfullscreen"
-                title={episodeId}
-                allow="picture-in-picture"
-                webkitallowfullscreen="true"></iframe>
+                  src={data}
+                  allowFullScreen="allowfullscreen"
+                  title={episodeId}
+                  allow="picture-in-picture"
+                  webkitallowfullscreen="true"
+                ></iframe>
               )}
             </div>
             <div className="epListContainer">
               <div className="epList">
-              {details?.episodes?.map((ep) => (
-                <Link to={`/watch/${ep.id}/${animeId}`} key={ep.id}>
-                  {ep.id === episodeId ? (
-                    <button className="active">{ep.number}</button>
-                  ) : (
-                    <button>{ep.number}</button>
-                  )}
-                </Link>
-              ))}
+                {details?.episodes?.map((ep) => (
+                  <Link to={`/watch/${ep.id}/${animeId}`} key={ep.id}>
+                    {ep.id === episodeId ? (
+                      <button className="active">{ep.number}</button>
+                    ) : (
+                      <button>{ep.number}</button>
+                    )}
+                  </Link>
+                ))}
               </div>
             </div>
           </div>
@@ -101,5 +103,5 @@ export default function AnimeStream() {
       </div>
       {/* <DiscussionPage /> */}
     </>
-  )
+  );
 }
