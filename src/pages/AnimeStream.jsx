7472e2ff46aedab981a3a64c3ab7a59d4b3bdf2c
Ahmer Gondal <ajg2204@hsutx.edu>
2023-10-12 22:17:34 -0500

    Handling error for new API

--- src/pages/AnimeStream.jsx
+++ src/pages/AnimeStream.jsx
@@ -1,83 +1,98 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./../components/css/AnimeStream.css";
-// import DiscussionPage from "./DiscussionPage";
-import { AniApi } from "../components/api";
 
 const AnimeStream = () => {
-  const { episodeId, animeId } = useParams();
-  const [animeDetails, setAnimeDetails] = useState({});
-  const [streamData, setStreamData] = useState({});
-  const [displayMain, setDisplayMain] = useState(true); // state to toggle between main and backup stream
+  const { animeId, episodeId } = useParams();
+
+  const [streamData, setStreamData] = useState({
+    info: {
+      title: "",
+      episode: "",
+      id: "",
+    },
+    stream: {
+      plyr: {
+        main: "",
+        backup: "",
+      },
+      multi: {
+        main: {
+          url: "",
+          isM3U8: false,
+          quality: "",
+        },
+        backup: {
+          url: "",
+          isM3U8: false,
+          quality: "",
+        },
+      },
+      tracks: {
+        file: "",
+        kind: "",
+      },
+      iframe: {
+        default: "",
+        backup: "",
+      },
+      nspl: {
+        main: "",
+        backup: "",
+      },
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const getAnimeDetails = async () => {
+    const fetchStreamData = async () => {
       try {
+        // Replace with your actual API endpoint
         const response = await axios.get(
-          `${AniApi}/meta/anilist/info/${animeId}`
+          `https://api.example.com/anime/${animeId}/episode/${episodeId}`
         );
-        setAnimeDetails(response.data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    const getAnimeStream = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.amvstr.me/api/v2/stream/${episodeId}`
-        );
-        if (response.data.code === 200) {
+        if (response.data && response.data.code === 200) {
           setStreamData(response.data);
+          setIsLoading(false);
         } else {
-          throw new Error("Could not retrieve anime stream data");
+          // handle error response
         }
-      } catch (err) {
-        console.error(err);
+      } catch (error) {
+        // handle errors during the request
       }
     };
 
-    getAnimeDetails();
-    getAnimeStream();
-  }, [animeId, episodeId]); // dependencies for the useEffect
+    fetchStreamData();
+  }, [animeId, episodeId]);
 
-  const toggleStream = () => {
-    setDisplayMain((prev) => !prev); // toggle between main and backup stream
-  };
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="animeStream">
       <div className="animeStreamContainer">
-        {/* Conditional rendering to handle if the data is not yet received */}
-        {streamData.info && (
-          <>
-            <div className="animeTitle">
-              <span>{streamData.info.title}</span>
-            </div>
-            <div className="animeVideo">
-              <iframe
-                src={
-                  displayMain
-                    ? streamData.stream.plyr.main
-                    : streamData.stream.plyr.backup
-                }
-                frameBorder="0"
-                allowFullScreen={true}
-                title={`Episode ${streamData.info.episode}`}
-                allow="autoplay; picture-in-picture"
-                webkitallowfullscreen="true"
-                mozallowfullscreen="true"
-              ></iframe>
-            </div>
-            <button onClick={toggleStream}>
-              Switch to {displayMain ? "Backup" : "Main"} Stream
-            </button>
-            {/* Other components */}
-          </>
-        )}
+        <div className="animeTitle">
+          <h1>
+            {streamData.info.title} - Episode {streamData.info.episode}
+          </h1>
+        </div>
+        <div className="animeVideo">
+          {/* The iframe src URL can be streamData.stream.iframe.default, streamData.stream.plyr.main, or any other URL you prefer. Adjust as necessary. */}
+          <iframe
+            src={streamData.stream.plyr.main}
+            frameBorder="0"
+            allowFullScreen={true}
+            title={`${streamData.info.title} Episode ${streamData.info.episode}`}
+            allow="autoplay; picture-in-picture"
+            webkitallowfullscreen="true"
+            mozallowfullscreen="true"
+          ></iframe>
+        </div>
+        {/* Add any additional components or information you want to render */}
       </div>
-      {/* <DiscussionPage /> */}
     </div>
   );
 };
