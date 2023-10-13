6c6f41854eadc64ebe3ed24a76eef23ad7ed3820
Ahmer Gondal <ajg2204@hsutx.edu>
2023-10-12 22:29:27 -0500

    import

--- src/pages/AnimeStream.jsx
+++ src/pages/AnimeStream.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./../components/css/AnimeStream.css";
